242d30159d648c2cf6271e0e14e843cdd0b3086e
carterfj <johnsoncarter150@gmail.com>
2025-04-15 20:31:48 -0600

    part 1-2, 2-5

--- retailData.py
+++ retailData.py
@@ -16,6 +16,14 @@ while True:
             salesData = pd.read_excel('Retail_Sales_Data.xlsx')
 
             #Part 1-2:
+            separatedNames = salesData["name"].str.split("_", expand=True)
+            
+            # Insert the new columns into the dataframe
+            salesData.insert(1, "first_name", separatedNames[0])
+            salesData.insert(2, "last_name", separatedNames[1])
+            
+            # Delete the original name column
+            del salesData["name"]
 
             #Part 1-3:
 
@@ -29,15 +37,25 @@ while True:
             print("The following are all the categories that have been sold:")
 
             #Part 2-2:
-
-            #Part 2-3: Ask the user for a category number.
-            selectedCategory = input("Please enter the number of the category you want to see summarized: ")
-
-            #Part 2-4:
-
-            #Part 2-5:
             
 
+            #Part 2-4:
+            
+            #Part 2-5: Create and display a bar chart
+                    # Group by product and sum the total_price
+            dfProductSales = dfFiltered.groupby('product')['total_price'].sum()
+                    
+                    # Create the chart
+            dfProductSales.plot(kind='bar')  # creates the chart
+            plot.title(f"Total Sales in {requestedCategory}")  # adds title to the top of the chart
+            plot.xlabel("Product")  # label for the x-axis
+            plot.ylabel("Total Sales")  # label for the y-axis
+            plot.show()  # makes the chart pop up on the screen
+        else:
+                    print("Invalid category number.")
+    except ValueError:
+                print("Please enter a valid number.")
+            
         #Exit the program if the user enters anything other than 1 or 2
         else:
             print("Closing the program.")
